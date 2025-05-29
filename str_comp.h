b6e59990f450f4e11c0c9a012520c69734ad20f2
rodrigoo-r <2nw53j32643y@gmail.com>
2025-05-28 20:45:42 -0500

    Update boolean definitions in str_comp.h for improved compatibility

--- str_comp.h
+++ str_comp.h
@@ -17,12 +17,10 @@ extern "C"
 #endif
 
  // Define true/false values
-#ifndef TRUE
-#define TRUE 1
-#endif
-
-#ifndef FALSE
-#define FALSE 0
+#ifndef FLUENT_LIBC_RELEASE
+#   include <std_bool.h>
+#else
+#   include <fluent/std_bool/std_bool.h>
 #endif
 
 /**
